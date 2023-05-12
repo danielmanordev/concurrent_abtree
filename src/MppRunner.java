99f1959b49dec2d2847273663de9deffa5bc1d4c
Daniel Manor <daniel.manor87@gmail.com>
2023-05-12 12:14:38 +0300

    print average

--- src/MppRunner.java
+++ src/MppRunner.java
@@ -1,14 +1,17 @@
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.OptionalDouble;
 import java.util.concurrent.ExecutionException;
 
 public class MppRunner {
 
     public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
         int dataRange = 10000;
-        int numberOfThreads = 12;
+        int numberOfThreads = 8;
         int a = 2;
         int b = 4;
-        int numberOfTests = 32;
+        int numberOfTests = 8;
+        ArrayList<Long> adds = new ArrayList();
         for (int i = 0; i < numberOfTests; i++) {
 
             Set concurrentSet = new OCCABTree(a, b);
@@ -29,12 +32,24 @@ public class MppRunner {
             System.out.println("Total adds:               " + testResult.TotalAdds.longValue());
             System.out.println("Total removes:            " + testResult.TotalRemoves.longValue());
             System.out.println("Total contains:           " + testResult.TotalContains.longValue());
-            System.out.println("Adds per microsecond:     " + testResult.TotalAdds.longValue() / (timeElapsedMicroseconds));
-            System.out.println("Removes per microsecond:  " + testResult.TotalRemoves.longValue() / (timeElapsedMicroseconds));
-            System.out.println("Contains per microsecond: " + testResult.TotalContains.longValue() / (timeElapsedMicroseconds));
+            System.out.println("Adds/\u33B2:                  " + testResult.TotalAdds.longValue() / (timeElapsedMicroseconds));
+            System.out.println("Removes/\u33B2:               " + testResult.TotalRemoves.longValue() / (timeElapsedMicroseconds));
+            System.out.println("Contains/\u33B2:              " + testResult.TotalContains.longValue() / (timeElapsedMicroseconds));
             System.out.println("Threads:                  " + numberOfThreads);
             System.out.println("Total time:               " + timeElapsed + " milliseconds");
             b *=2;
+            if(i >1) {
+                adds.add(testResult.TotalAdds.longValue());
+            }
+        }
+        if(numberOfTests > 2){
+            double average = adds
+                    .stream()
+                    .mapToDouble(n -> n)
+                    .average().orElse(0.0);
+            average /= 1000;
+            int numberOfCalcTests = adds.size();
+            System.out.println("Adds/\u33B2 last " +numberOfCalcTests+ " tests average:  " + average);
         }
     }
 }
