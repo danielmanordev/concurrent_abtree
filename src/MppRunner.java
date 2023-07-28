7d7862a36929e9144721f906196e8747c292b372
Daniel Manor <daniel.manor87@gmail.com>
2023-07-28 20:35:08 +0300

    update MppRunner

--- src/MppRunner.java
+++ src/MppRunner.java
@@ -6,24 +6,37 @@ import java.util.concurrent.ExecutionException;
 public class MppRunner {
 
     public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+        int cores = Runtime.getRuntime().availableProcessors();
+
         int dataRange = 10000;
-        int numberOfThreads = 4;
+        int numberOfThreads = 8;
         int a = 2;
         int b = 16;
-        int numberOfTests = 3;
+        int numberOfTests = 96;
+        int testDuration=10000;
+        int perAdd=80;
+        int perContains=0;
+        int perDelete=20;
+        int perRemove=100-perAdd-perContains;
+        int perRange=0;
         ArrayList<Long> adds = new ArrayList();
+        System.out.println("Number of cores: "+cores);
+        System.out.println("Number of tests: "+numberOfTests);
+        System.out.println("Single test duration: "+testDuration+ " ms");
+        System.out.println("insert: "+perAdd+"%");
+        System.out.println("remove: "+perRemove+"%");
+        System.out.println("contains: "+perContains+"%");
+        System.out.println("range: "+perRange+"%");
+        System.out.println("Starting....");
         for (int i = 0; i < numberOfTests; i++) {
 
             Set concurrentSet = new OCCABTree(a, b, numberOfThreads);
-            concurrentSet.add(6,6);
             TestSet.seed(concurrentSet, dataRange, dataRange / 2);
-            concurrentSet.remove(6);
-            concurrentSet.scan(1, 1000);
 
             //nteger.parseInt(args[1]);
 
             long start = System.currentTimeMillis();
-            TestResult testResult = TestSet.runTest(concurrentSet, numberOfThreads, dataRange, 0, 50, 40,100000);
+            TestResult testResult = TestSet.runTest(concurrentSet, numberOfThreads, dataRange, perContains,perAdd ,perDelete, testDuration);
             long finish = System.currentTimeMillis();
             long timeElapsed = finish - start;
             long timeElapsedMicroseconds = timeElapsed * 1000;
@@ -35,20 +48,16 @@ public class MppRunner {
             System.out.println("Total adds:               " + testResult.TotalAdds.longValue());
             System.out.println("Total removes:            " + testResult.TotalRemoves.longValue());
             System.out.println("Total contains:           " + testResult.TotalContains.longValue());
-            System.out.println("Total scans:              " + testResult.TotalScans.longValue());
             System.out.println("Adds/\u33B2:                  " + testResult.TotalAdds.longValue() / (timeElapsedMicroseconds));
             System.out.println("Removes/\u33B2:               " + testResult.TotalRemoves.doubleValue() / (timeElapsedMicroseconds));
             System.out.println("Contains/\u33B2:              " + testResult.TotalContains.longValue() / (timeElapsedMicroseconds));
-            System.out.println("Scans/\u33B2:                 " + testResult.TotalScans.longValue() / (timeElapsedMicroseconds));
             System.out.println("Threads:                  " + numberOfThreads);
             System.out.println("Total time:               " + timeElapsed + " milliseconds");
-           // b *=2;
+            // numberOfThreads++;
             if(i >1) {
                 adds.add(testResult.TotalAdds.longValue());
             }
-           concurrentSet.scan(1,1000);
         }
-
         if(numberOfTests > 2){
             double average = adds
                     .stream()
