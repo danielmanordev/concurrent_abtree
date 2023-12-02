7e3829b8e786949bc3e15323ce4c2b4b7fd1c44f
Daniel Manor <daniel.manor87@gmail.com>
2023-12-02 23:09:39 +0200

    more tests

--- src/MppRunner.java
+++ src/MppRunner.java
@@ -29,8 +29,8 @@ public class MppRunner {
         mm.put(33, new LatestVersion(33,3,System.currentTimeMillis(),1));*/
 
         int dataRange = 1000000;
-        int numberOfThreads = 80;
-        int numberOfScanThreads = 1;//Integer.parseInt(args[0]);
+        int numberOfThreads = 1;
+        int numberOfScanThreads = 0;//Integer.parseInt(args[0]);
         int a = 2;
         int b = 128;
         int numberOfTests = 79;
@@ -75,8 +75,8 @@ public class MppRunner {
             System.out.println("Scan Threads:             " + numberOfScanThreads);
             System.out.println("Non Scan Threads:         " + (numberOfThreads-numberOfScanThreads));
             System.out.println("Total time:               " + timeElapsed + " milliseconds");
-            //numberOfThreads++;
-            numberOfScanThreads++;
+            numberOfThreads++;
+            //numberOfScanThreads++;
             if (i > 1) {
                 adds.add(testResult.TotalAdds.longValue());
             }
