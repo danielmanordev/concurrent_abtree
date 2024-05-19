24c6a656f7f708e87a0b0eb3261974f21a3dd7c6
Daniel Manor <daniel.manor87@gmail.com>
2024-05-19 14:58:24 +0300

    test scan

--- src/MppRunner.java
+++ src/MppRunner.java
@@ -29,15 +29,15 @@ public class MppRunner {
         mm.put(33, new LatestVersion(33,3,System.currentTimeMillis(),1));*/
 
         int dataRange = 1000000;
-        int numberOfThreads = 79;
-        int numberOfScanThreads = 1;//Integer.parseInt(args[0]);
+        int numberOfThreads = 12;
+        int numberOfScanThreads = 0;//Integer.parseInt(args[0]);
         int a = 2;
-        int b = 256;
-        int numberOfTests = 79;
+        int b = 128;
+        int numberOfTests = 6 ;
         int testDuration=10000;
-        int perAdd=80;
+        int perAdd=100;
         int perContains=0;
-        int perRemove=20;
+        int perRemove=0;
         /// int perRange=100-perAdd-perContains-perRemove;
         ArrayList<Long> adds = new ArrayList();
         System.out.println("Number of available processors: "+availableProcessors);
@@ -75,7 +75,7 @@ public class MppRunner {
             System.out.println("Scan Threads:             " + numberOfScanThreads);
             System.out.println("Non Scan Threads:         " + (numberOfThreads-numberOfScanThreads));
             System.out.println("Total time:               " + timeElapsed + " milliseconds");
-            // numberOfThreads++;
+            //numberOfThreads++;
             numberOfScanThreads++;
             if (i > 1) {
                 adds.add(testResult.TotalAdds.longValue());
